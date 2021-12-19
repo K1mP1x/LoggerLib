927e052a8d475ffdb55811bda09daae029b1f225
Kamil Prorok <kamilxprorok@gmail.com>
2021-12-19 13:46:03 +0100

    Path to tests changed

--- LoggerLib/Data/LoggerConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LoggerLib.Data
-{
-    public static class LoggerConfig
-    {
-        public static string LogsDir { get; set; } = "logs";
-
-        public static string LogsExtension { get; set; } = "log";
-    }
-}
--- LoggerTests/TestsSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LoggerTests
-{
-    public class TestsSettings
-    {
-        public const string LogsDir = "logging_dir";
-        public const string LogsExtension = "txt";
-    }
-}
