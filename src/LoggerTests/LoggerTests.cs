86cce4b81582841d202a514dd037f3d43da2dc21
Kamil Prorok <kamilxprorok@gmail.com>
2021-12-19 23:11:58 +0100

    test fix

--- src/LoggerTests/LoggerTests.cs
+++ src/LoggerTests/LoggerTests.cs
@@ -7,12 +7,6 @@ namespace LoggerTests
 {
     public class LoggerTests
     {
-        static LoggerTests()
-        {
-            if (Directory.Exists(TestsSettings.LogsDir))
-                Directory.Delete(TestsSettings.LogsDir, true);
-        }
-        
         public LoggerTests()
         {
             LoggerConfiguration.LogsDir = TestsSettings.LogsDir;
