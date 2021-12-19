1d73b0df2294d2b99f1cec67f76ab515d2f158c0
Kamil Prorok <kamilxprorok@gmail.com>
2021-12-19 14:11:05 +0100

    tests

---
+++ src/LoggerTests/LoggerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using Logger.Data.Configuration;
+using Xunit;
+
+namespace LoggerTests
+{
+    public class LoggerTests
+    {
+        static LoggerTests()
+        {
+            if (Directory.Exists(TestsSettings.LogsDir))
+                Directory.Delete(TestsSettings.LogsDir, true);
+        }
+        
+        public LoggerTests()
+        {
+            LoggerConfiguration.LogsDir = TestsSettings.LogsDir;
+            LoggerConfiguration.LogsExtension = TestsSettings.LogsExtension;
+            Logger.Logger.Init();
+        }
+        
+        [Fact]
+        public void Init_WhenInitialize_CreatesLogDirectory()
+        {
+            Directory.Exists(TestsSettings.LogsDir).Should().BeTrue();
+        }
+    }
+}
